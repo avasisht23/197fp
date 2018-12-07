e97a9f6768cd196cf7146daa78948722348df2fd
Ajay Vasisht <AjayVasisht23@Sureshs-MacBook-Pro.local>
2018-12-07 02:00:24 -0500

    syntax fix

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res)) {
+app.get('/', function(req, res) {
   res.send('Hello Youtube')
 });
 
-app.get('/webhook', function(req, res)) {
-  if(req.query['hub.verify_token'] === ('197fp') {
+app.get('/webhook', function(req, res) {
+  if(req.query['hub.verify_token'] === ('197fp')) {
     res.send(req.query['hub.challenger']);
   }
   res.send('No entry');
@@ -22,4 +22,4 @@ app.get('/webhook', function(req, res)) {
 
 app.listen(process.env.PORT || 5000, function () {
   console.log('App listening on port ' + app.get('port'))
-})
+});
