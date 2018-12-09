0ad6917fb6e5a5a37018c97f6f905f553e4bf50a
Ajay Vasisht <AjayVasisht23@huntsman-ve502-0133.apn.wlan.upenn.edu>
2018-12-09 16:05:48 -0500

    h

--- index.js
+++ index.js
@@ -85,10 +85,10 @@ function handleMessage(sender_psid, received_message) {
   if (received_message.text) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
     if (received_message.text === "Hello")
-    // response = {
-    //   "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-    // }
     response = {
       "attachment": {
         "type": "template",
@@ -202,6 +202,7 @@ function handlePostback(sender_psid, received_postback) {
 
 function callSendAPI(sender_psid, response) {
   // Construct the message body
+  console.log("resp", response)
   let request_body = {
     "recipient": {
       "id": sender_psid
