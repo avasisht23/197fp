bec3a3d92f5665009948749bb3497f0b7c800276
Ajay Vasisht <AjayVasisht23@huntsman-ve507-0648.apn.wlan.upenn.edu>
2018-12-10 16:28:49 -0500

    d

--- callSendApi.js
+++ callSendApi.js
@@ -5,6 +5,9 @@ var mongoose = require('mongoose')
 var request = require('request')
 var app = express();
 
+var token = process.env.FB_VERIFY_TOKEN;
+var access = process.env.FB_ACCESS_TOKEN;
+
 var callSendAPI = function(sender_psid, response) {
   // Construct the message body
   console.log("resp", response)
